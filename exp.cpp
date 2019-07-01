f11bfee0e2ee2403fcf7ab66507ab0dc014c0d7a
kshitij-agarwal <kshitijagarwal902@gmail.com>
2019-07-01 18:01:06 +0530

    Just Experimenting!!

---
+++ exp.cpp
@@ -0,0 +1,24 @@
+//  This is a file to test basics of C++
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <cstdio>
+#include <stdlib.h>
+#include <typeinfo>
+
+using namespace std;
+
+int main()
+{
+    string x = "A1";
+    int temp_dig = x[1];
+    int temp_alpha = (toupper(x[0]));
+    cout << temp_alpha << "\n"
+         << temp_dig;
+
+    cout << endl << endl;
+    return 0;
+}
